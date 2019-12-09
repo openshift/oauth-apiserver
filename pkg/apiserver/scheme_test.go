cce5d083d0785c3309b1f1376f1b121190e86abe
David Eads <deads@redhat.com>
2019-12-09 08:41:02 -0500

    enable fuzzer tests

--- pkg/apiserver/scheme_test.go
+++ pkg/apiserver/scheme_test.go
@@ -5,8 +5,13 @@ import (
 
 	"github.com/openshift/oauth-apiserver/pkg/serverscheme"
 	"k8s.io/apimachinery/pkg/api/apitesting/roundtrip"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 )
 
+var fuzzerFuncs = func(codecs serializer.CodecFactory) []interface{} {
+	return []interface{}{}
+}
+
 func TestRoundTripTypes(t *testing.T) {
-	roundtrip.RoundTripTestForScheme(t, serverscheme.Scheme)
+	roundtrip.RoundTripTestForScheme(t, serverscheme.Scheme, fuzzerFuncs)
 }
