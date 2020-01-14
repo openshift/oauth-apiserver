b7e5ef0a8ddf8145d21463ca4c44760c983e9010
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-01-14 15:51:44 +0100

    Add API group REST tests

--- pkg/cmd/oauth-apiserver/cmd_test.go
+++ pkg/cmd/oauth-apiserver/cmd_test.go
@@ -3,7 +3,11 @@ package oauth_apiserver_test
 import (
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	etcdtesting "k8s.io/apiserver/pkg/storage/etcd3/testing"
+
+	oauthclient "github.com/openshift/client-go/oauth/clientset/versioned"
+	userclient "github.com/openshift/client-go/user/clientset/versioned"
 
 	oauthapiservertesting "github.com/openshift/oauth-apiserver/pkg/cmd/oauth-apiserver/testing"
 )
@@ -12,9 +16,25 @@ func TestServerStartUp(t *testing.T) {
 	etcd, storageConfig := etcdtesting.NewUnsecuredEtcd3TestClientServer(t)
 	defer etcd.Terminate(t)
 
-	tearDown, _, err := oauthapiservertesting.StartDefaultTestServer(t, storageConfig)
+	tearDown, config, err := oauthapiservertesting.StartDefaultTestServer(t, storageConfig)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer tearDown()
+
+	oauthClient, err := oauthclient.NewForConfig(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := oauthClient.OauthV1().OAuthAccessTokens().List(metav1.ListOptions{}); err != nil {
+		t.Fatal(err)
+	}
+
+	userClient, err := userclient.NewForConfig(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := userClient.UserV1().Groups().List(metav1.ListOptions{}); err != nil {
+		t.Fatal(err)
+	}
 }
