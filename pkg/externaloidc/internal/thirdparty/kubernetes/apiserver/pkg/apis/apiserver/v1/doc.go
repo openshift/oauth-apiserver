ea03fc4ad43f63c5b5d90da2c6e0bd07f8fb8761
Bryce Palmer <bpalmer@redhat.com>
2026-03-04 11:59:57 -0500

    UPSTREAM: <carry>: externaloidc/internal: wire up generators
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

---
+++ pkg/externaloidc/internal/thirdparty/kubernetes/apiserver/pkg/apis/apiserver/v1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=github.com/openshift/oauth-apiserver/pkg/externaloidc/internal/thirdparty/kubernetes/apiserver/pkg/apis/apiserver
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=apiserver.config.k8s.io
+
+package v1
---
+++ pkg/externaloidc/internal/thirdparty/kubernetes/apiserver/pkg/apis/apiserver/v1/register.go
@@ -0,0 +1,5 @@
+package v1
+
+import "k8s.io/apimachinery/pkg/runtime"
+
+var localSchemeBuilder = runtime.NewSchemeBuilder()
